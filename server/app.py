a51afc3f6aebaa4d32319af5921f41e6079c46fc
dader34 <56900960+dader34@users.noreply.github.com>
2023-11-12 17:15:41 -0800

    Passed all tests

--- server/app.py
+++ server/app.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route("/print/<parameter>")
+def printroute(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<parameter>')
+def count(parameter):
+    c = ""
+    for x in range(int(parameter)):
+        c += f'{x}\n'
+    return c
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1,operation,num2):
+    n1 = int(num1)
+    n2 = int(num2)
+    if(operation == "+"):
+        return str(n1 + n2)
+    elif operation == "-":
+        return str(n1-n2)
+    elif operation == "*":
+        return str(n1*n2)
+    elif operation == "div":
+        return str(n1/n2)
+    elif operation == "%":
+        return str(n1 % n2)
+    return 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
